bb2fd27a47431b8961683151fd305bf26ebca879
Meowgle / MNG <66216330+meowgle@users.noreply.github.com>
2022-02-23 18:53:07 -0600

    Update program-template.asm

--- program-template.asm
+++ program-template.asm
@@ -1,4 +1,4 @@
    bits 16     ; We're dealing with 16 bit code
-   org 0x7c00  ; Inform the assembler of the starting location for this code
+   org 0x7e00  ; Inform the assembler of the starting location for this code
    
    prog_start:
